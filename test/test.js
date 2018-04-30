de90bcc451dcf1b902583223354e89c59a829922
Egor Suvorov <egor.suvorov@gmail.com>
2018-04-30 17:43:57 +0300

    Add some comments

--- test/test.js
+++ test/test.js
@@ -4,14 +4,14 @@ contract("Foo", async () => {
   it("emits foo", async () => {
     const foo = await Foo.new();
     const res = web3.eth.getTransactionReceipt(foo.transactionHash);
-    assert.equal(res.logs[0].event, 'FooEvent');
+    assert.equal(res.logs[0].event, 'FooEvent');  // Fails, no information about FooEvent whatsoever
   });
 
   it("emits bar", async () => {
     const foo = await Foo.new();
     const res1 = await foo.emitBar();
     const res2 = web3.eth.getTransactionReceipt(res1.tx);
-    assert.equal(res1.logs[0].event, 'BarEvent');
-    assert.equal(res2.logs[0].event, 'BarEvent');
+    assert.equal(res1.logs[0].event, 'BarEvent');  // Succeeds
+    assert.equal(res2.logs[0].event, 'BarEvent');  // Fails, no information about FooEvent whatsoever
   });
 });
